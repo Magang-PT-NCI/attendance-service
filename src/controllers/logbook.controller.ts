caf8023f3efca74f5f39ce6a3cce49f037954ce2
iqmalakur <iakbarkurnia@gmail.com>
2024-09-28 06:48:20 +0700

    logbook post endpoint

--- src/controllers/logbook.controller.ts
+++ src/controllers/logbook.controller.ts
@@ -1,14 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
 import { LogbookService } from '../services/logbook.service';
-import { ApiSecurity } from '@nestjs/swagger';
+import { ApiSecurity, ApiTags } from '@nestjs/swagger';
+import { LogbookReqBody, LogbookResBody } from '../dto/logbook.dto';
+import { logFormat, logger } from '../utils/logger.utils';
+import { ActivityStatus } from '@prisma/client';
+import { ApiLogbookPost } from '../decorators/api-logbook.decorator';
 
 @Controller()
 @ApiSecurity('jwt')
+@ApiTags('Logbook')
 export class LogbookController {
   constructor(private readonly service: LogbookService) {}
 
-  async postLogbook() {
-    return this.service.handlePostLogbook();
+  @Post('/logbook')
+  @ApiLogbookPost()
+  async postLogbook(@Body() reqBody: LogbookReqBody): Promise<LogbookResBody> {
+    const data: LogbookReqBody = {
+      attendance_id: reqBody.attendance_id,
+      description: reqBody.description,
+      status: reqBody.status?.toLowerCase() as ActivityStatus,
+      start_time: reqBody.start_time,
+      end_time: reqBody.end_time,
+    };
+
+    logger.debug(`request body: ${logFormat(data)}`);
+
+    // check all required fields
+    for (const [field, value] of Object.entries(data)) {
+      if (!value && value !== 0) {
+        throw new BadRequestException(`${field} harus diisi`);
+      }
+    }
+
+    if (data.status !== 'progress' && data.status !== 'done') {
+      throw new BadRequestException(`status '${data.status}' tidak valid`);
+    }
+
+    return this.service.handlePostLogbook(data);
   }
 
   async getLogbook() {
--- src/services/logbook.service.ts
+++ src/services/logbook.service.ts
@@ -1,9 +1,78 @@
-import { Injectable, NotImplementedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  NotImplementedException,
+} from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { getPrismaClient } from '../utils/prisma.utils';
+import { LogbookResBody, LogbookReqBody } from '../dto/logbook.dto';
+import { DateUtils } from '../utils/date.utils';
+import { PrismaActivity } from '../interfaces/logbook.interfaces';
+import { logFormat, logger } from '../utils/logger.utils';
 
 @Injectable()
 export class LogbookService {
-  async handlePostLogbook() {
-    throw new NotImplementedException();
+  private prisma: PrismaClient;
+
+  constructor() {
+    this.prisma = getPrismaClient();
+  }
+
+  async handlePostLogbook(data: LogbookReqBody): Promise<LogbookResBody> {
+    const { attendance_id, description, status, start_time, end_time } = data;
+
+    let attendance: { id: number };
+    try {
+      attendance = await this.prisma.attendance.findFirst({
+        where: { id: attendance_id },
+        select: { id: true },
+      });
+    } catch (error) {
+      logger.error(logFormat(error));
+      throw new InternalServerErrorException();
+    }
+
+    if (!attendance) {
+      throw new NotFoundException('data attendance tidak ditemukan');
+    }
+
+    if (!DateUtils.isValidTime(start_time)) {
+      throw new BadRequestException(
+        'start_time harus waktu yang valid dengan format HH:MM',
+      );
+    }
+
+    if (!DateUtils.isValidTime(end_time)) {
+      throw new BadRequestException(
+        'end_time harus waktu yang valid dengan format HH:MM',
+      );
+    }
+
+    try {
+      const logbook: PrismaActivity = await this.prisma.activity.create({
+        data: {
+          attendance_id,
+          description,
+          status,
+          start_time: DateUtils.setDate(start_time).getTimeIso(),
+          end_time: DateUtils.setDate(end_time).getTimeIso(),
+        },
+        select: {
+          id: true,
+          description: true,
+          status: true,
+          start_time: true,
+          end_time: true,
+        },
+      });
+
+      return new LogbookResBody(logbook);
+    } catch (error) {
+      logger.error(logFormat(error));
+      throw new InternalServerErrorException();
+    }
   }
 
   async handleGetLogbook() {
