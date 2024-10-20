cfb7c4dc7dd820f3e84ccdae9a4cead98a5f1b47
iqmalakur <iakbarkurnia@gmail.com>
2024-10-20 08:19:43 +0700

    restore date utils unit test

--- src/tests/mocks/date.utils.mock.ts
+++
@@ -1,7 +0,0 @@
-jest.mock('../../utils/date.utils', () => ({
-  getDateString: jest.fn().mockReturnValue('2024-01-01'),
-  getTimeString: jest.fn().mockReturnValue('10:00'),
-  getLogDateFormat: jest.fn().mockReturnValue('10:00:00.000'),
-  isValidTime: jest.fn().mockReturnValue(true),
-  getDate: jest.fn().mockReturnValue(new Date()),
-}));
