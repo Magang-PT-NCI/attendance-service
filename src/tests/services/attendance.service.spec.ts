a3fe462e6896077132ad63ce90d43427927c13c3
iqmalakur <iakbarkurnia@gmail.com>
2024-11-07 08:01:48 +0700

    complete attendance service unit test

--- src/tests/services/attendance.service.spec.ts
+++ src/tests/services/attendance.service.spec.ts
@@ -8,11 +8,14 @@ import {
 } from '@nestjs/common';
 import { getDate } from '../../utils/date.utils';
 import {
+  AttendanceConfirmationResBody,
   AttendancePostReqBody,
   AttendancePostResBody,
   AttendanceResBody,
+  OvertimeResBody,
 } from '../../dto/attendance.dto';
 import { uploadFile } from '../../utils/upload.utils';
+import { ConfirmationType } from '@prisma/client';
 
 jest.mock('../../utils/api.utils');
 jest.mock('../../utils/date.utils');
@@ -20,8 +23,11 @@ jest.mock('../../utils/upload.utils');
 jest.mock('../../dto/attendance.dto');
 jest.mock('../../services/prisma.service', () => ({
   PrismaService: jest.fn().mockImplementation(() => ({
-    attendance: { findFirst: jest.fn(), create: jest.fn() },
+    attendance: { findFirst: jest.fn(), create: jest.fn(), update: jest.fn() },
+    attendanceConfirmation: { findFirst: jest.fn(), create: jest.fn() },
     check: { create: jest.fn() },
+    overtime: { create: jest.fn() },
+    $transaction: jest.fn(),
   })),
 }));
 
@@ -29,9 +35,38 @@ describe('attendance service test', () => {
   let service: AttendanceService;
   let prisma: PrismaService;
 
-  beforeEach(() => {
+  const attendanceData: AttendancePostReqBody = {
+    nik: '12345',
+    type: 'check_in',
+    location: { latitude: 123, longitude: 123 },
+    photo: {
+      originalname: 'image.png',
+      mimetype: 'image/png',
+      buffer: Buffer.from('test'),
+    } as Express.Multer.File,
+  };
+
+  const mockDate = (timeString: string) => {
+    (getDate as jest.Mock).mockImplementationOnce(() => {
+      const date = new Date(`2024-01-01T${timeString}:00.000Z`);
+      date.setHours(date.getHours() - 7);
+      return date;
+    });
+    (getDate as jest.Mock).mockReturnValueOnce(new Date('2024-01-01'));
+    (getDate as jest.Mock).mockReturnValueOnce(
+      new Date(`1970-01-01T${timeString}:00.000Z`),
+    );
+  };
+
+  beforeAll(() => {
     prisma = new PrismaService();
     service = new AttendanceService(prisma);
+
+    (prisma.$transaction as jest.Mock).mockImplementation(
+      (callback: (prisma: PrismaService) => any) => {
+        return callback(prisma);
+      },
+    );
   });
 
   afterEach(() => {
@@ -86,56 +121,26 @@ describe('attendance service test', () => {
         select: expect.anything(),
       });
     });
+
+    it('should handle prisma error', async () => {
+      (getEmployee as jest.Mock).mockReturnValue({ nik });
+      (prisma.attendance.findFirst as jest.Mock).mockRejectedValue(new Error());
+
+      await expect(
+        service.handleGetAttendance(nik, filter, date),
+      ).rejects.toThrow(new InternalServerErrorException());
+      expect(getEmployee).toHaveBeenCalledWith(nik);
+    });
   });
 
   describe('handleCheckIn test', () => {
-    const attendanceData: AttendancePostReqBody = {
-      nik: '12345',
-      type: 'check_in',
-      location: { latitude: 123, longitude: 123 },
-      photo: {
-        originalname: 'image.png',
-        mimetype: 'image/png',
-        buffer: Buffer.from('test'),
-      } as Express.Multer.File,
-    };
-
-    const mockDate = (timeString: string) => {
-      (getDate as jest.Mock).mockImplementationOnce(() => {
-        const date = new Date(`2024-01-01T${timeString}:00.000Z`);
-        date.setHours(date.getHours() - 7);
-        return date;
-      });
-      (getDate as jest.Mock).mockReturnValueOnce(new Date('2024-01-01'));
-      (getDate as jest.Mock).mockReturnValueOnce(
-        new Date(`1970-01-01T${timeString}:00.000Z`),
-      );
-    };
-
     it('should throw InternalServerError when network or database problem occurred', async () => {
-      const callHandler = async () => {
-        mockDate('06:30');
-        await expect(service.handleCheckIn(attendanceData)).rejects.toThrow(
-          new InternalServerErrorException(),
-        );
-      };
-
       (prisma.attendance.findFirst as jest.Mock).mockRejectedValue(new Error());
-      await callHandler();
+      mockDate('06:30');
 
-      (prisma.attendance.findFirst as jest.Mock).mockReturnValue(null);
-      (uploadFile as jest.Mock).mockRejectedValue(
+      await expect(service.handleCheckIn(attendanceData)).rejects.toThrow(
         new InternalServerErrorException(),
       );
-      await callHandler();
-
-      (uploadFile as jest.Mock).mockReturnValue('image.png');
-      (prisma.check.create as jest.Mock).mockRejectedValue(new Error());
-      await callHandler();
-
-      (prisma.check.create as jest.Mock).mockReturnValue({ id: 1 });
-      (prisma.attendance.create as jest.Mock).mockRejectedValue(new Error());
-      await callHandler();
     });
 
     it('should throw ConflictException when check in time is invalid', async () => {
@@ -202,6 +207,240 @@ describe('attendance service test', () => {
           date: '2024-01-01T00:00:00.000Z',
           status: 'presence',
         },
+        select: expect.any(Object),
+      });
+    });
+  });
+
+  describe('handleCheckOut test', () => {
+    const attendance = {
+      checkIn: { time: '06:10' },
+      check_out_id: null,
+      activities: [{ id: 1 }],
+      overtime_id: null,
+    };
+
+    it('should throw InternalServerError when network or database problem occurred', async () => {
+      (prisma.attendance.findFirst as jest.Mock).mockRejectedValue(new Error());
+      mockDate('14:05');
+
+      await expect(service.handleCheckOut(attendanceData)).rejects.toThrow(
+        new InternalServerErrorException(),
+      );
+    });
+
+    it('should throw ConflictException if employee does not checked in first', async () => {
+      for (const attendance of [null, { checkIn: null }]) {
+        (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(
+          attendance,
+        );
+        mockDate('14:05');
+
+        await expect(service.handleCheckOut(attendanceData)).rejects.toThrow(
+          new ConflictException('tidak dapat check out sebelum check in'),
+        );
+      }
+    });
+
+    it('should throw ConflictException if employee have been checked out', async () => {
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue({
+        checkIn: { time: '06:10' },
+        check_out_id: 2,
+      });
+      mockDate('14:05');
+
+      await expect(service.handleCheckOut(attendanceData)).rejects.toThrow(
+        new ConflictException('check out telah dilakukan'),
+      );
+    });
+
+    it('should throw ConflictException if employee does not fill the logbooks', async () => {
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue({
+        checkIn: { time: '06:10' },
+        check_out_id: null,
+        activities: [],
+      });
+      mockDate('14:05');
+
+      await expect(service.handleCheckOut(attendanceData)).rejects.toThrow(
+        new ConflictException('harus mengisi logbook terlebih dahulu'),
+      );
+    });
+
+    it('should throw ConflictException if employee perform check out outside the time', async () => {
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+      mockDate('17:00');
+
+      await expect(service.handleCheckOut(attendanceData)).rejects.toThrow(
+        new ConflictException(
+          'tidak dapat melakukan check out setelah pukul 15:00',
+        ),
+      );
+
+      attendance.overtime_id = 1;
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+      mockDate('20:00');
+
+      await expect(service.handleCheckOut(attendanceData)).rejects.toThrow(
+        new ConflictException(
+          'tidak dapat melakukan check out setelah pukul 19:00',
+        ),
+      );
+    });
+
+    it('should return success to perform check out', async () => {
+      attendance.overtime_id = null;
+      attendance.check_out_id = null;
+      mockDate('14:05');
+
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+      (prisma.attendance.update as jest.Mock).mockResolvedValue({ id: 1 });
+      (prisma.check.create as jest.Mock).mockResolvedValue({ id: 2 });
+      (uploadFile as jest.Mock).mockResolvedValue('image.png');
+      (AttendancePostResBody as jest.Mock).mockReturnValue({
+        id: 1,
+        status: 'presence',
+      });
+
+      expect(await service.handleCheckOut(attendanceData)).toEqual({
+        id: 1,
+        status: 'presence',
+      });
+      expect(uploadFile).toHaveBeenCalledWith(
+        attendanceData.photo,
+        attendanceData.nik,
+        attendanceData.type,
+      );
+      expect(prisma.check.create).toHaveBeenCalledWith({
+        data: {
+          type: 'out',
+          time: '1970-01-01T14:05:00.000Z',
+          location: '123,123',
+          photo: 'image.png',
+        },
+      });
+      expect(prisma.attendance.update).toHaveBeenCalledWith({
+        where: expect.any(Object),
+        select: expect.any(Object),
+        data: { check_out_id: 2 },
+      });
+    });
+  });
+
+  describe('handleOvertime test', () => {
+    const nik = '123456';
+    const attendance = {
+      id: 1,
+      checkIn: { time: '06:10' },
+      check_out_id: null,
+      activities: [{ id: 1 }],
+      overtime_id: null,
+    };
+
+    it('should throw ConflictException if current date does not valid', async () => {
+      mockDate('13:00');
+
+      await expect(service.handleOvertime(nik)).rejects.toThrow(
+        new ConflictException(
+          'konfirmasi lembur hanya dapat dilakukan pada pukul 14:00 hingga 15:00',
+        ),
+      );
+    });
+
+    it('should throw ConflictException if employee does not checked in', async () => {
+      mockDate('14:10');
+      attendance.checkIn = null;
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+
+      await expect(service.handleOvertime(nik)).rejects.toThrow(
+        new ConflictException(
+          'tidak dapat melakukan konfirmasi lembur karena belum melakukan check in',
+        ),
+      );
+    });
+
+    it('should throw ConflictException if employee have been checked out', async () => {
+      mockDate('14:10');
+      attendance.checkIn = { time: '06:10' };
+      attendance.check_out_id = 2;
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+
+      await expect(service.handleOvertime(nik)).rejects.toThrow(
+        new ConflictException(
+          'tidak dapat melakukan konfirmasi lembur karena telah melakukan check out',
+        ),
+      );
+    });
+
+    it('should throw ConflictException if employee already have overtime', async () => {
+      mockDate('14:10');
+      attendance.checkIn = { time: '06:10' };
+      attendance.check_out_id = null;
+      attendance.overtime_id = 1;
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+
+      await expect(service.handleOvertime(nik)).rejects.toThrow(
+        new ConflictException('telah melakukan konfirmasi lembur'),
+      );
+    });
+
+    it('should success to request overtime', async () => {
+      mockDate('14:10');
+      attendance.checkIn = { time: '06:10' };
+      attendance.check_out_id = null;
+      attendance.overtime_id = null;
+
+      (prisma.attendance.findFirst as jest.Mock).mockResolvedValue(attendance);
+      (prisma.overtime.create as jest.Mock).mockResolvedValue({ id: 1 });
+      (prisma.attendance.update as jest.Mock).mockResolvedValue({ id: 1 });
+      (OvertimeResBody as jest.Mock).mockReturnValue({ id: 1 });
+
+      expect(await service.handleOvertime(nik)).toEqual({ id: 1 });
+      expect(prisma.overtime.create).toHaveBeenCalledWith({
+        data: { approved: false, checked: false },
+      });
+      expect(prisma.attendance.update).toHaveBeenCalledWith({
+        where: { id: 1 },
+        data: { overtime_id: 1 },
+      });
+    });
+  });
+
+  describe('handleAttendanceConfirmation test', () => {
+    const confirmation = {
+      attendance_id: 1,
+      type: 'check_in' as ConfirmationType,
+      description: 'lorem ipsum',
+      reason: null,
+      attachment: {
+        originalname: 'image.png',
+        mimetype: 'image/png',
+        buffer: Buffer.from('test'),
+      } as Express.Multer.File,
+    };
+
+    it('should throw ConflictException if there is existing confirmation', async () => {
+      (prisma.attendanceConfirmation.findFirst as jest.Mock).mockResolvedValue({
+        id: 1,
+      });
+
+      await expect(
+        service.handleAttendanceConfirmation(confirmation),
+      ).rejects.toThrow(
+        new ConflictException(
+          'Anda masih memiliki konfirmasi yang belum diperiksa!',
+        ),
+      );
+    });
+
+    it('should success to request attendance confirmation', async () => {
+      (prisma.attendanceConfirmation.findFirst as jest.Mock).mockResolvedValue(
+        null,
+      );
+      (AttendanceConfirmationResBody as jest.Mock).mockReturnValue({ id: 1 });
+
+      expect(await service.handleAttendanceConfirmation(confirmation)).toEqual({
+        id: 1,
       });
     });
   });
